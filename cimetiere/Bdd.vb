b2ab2ff9f89a608de80d4373c25df3a2f4ec60ac
bastv212 <bastv212@gmail.com>
2017-06-09 00:07:59 +0200

    màj

--- cimetiere/Bdd.vb
+++ cimetiere/Bdd.vb
@@ -5,8 +5,6 @@ Imports System
 Imports System.Net
 Imports System.IO
 Imports System.Text
-Imports cimetiere.EntitesTableAdapters
-Imports cimetiere.Entites
 
 Module Bdd
     Private Const ConnStr As String = "Server=" & ConfigLocale.BddHost & ";User Id=" & ConfigLocale.BddUser & "; Password=" & ConfigLocale.BddPass & "; Database=" & ConfigLocale.BddNom & "; Pooling=false"
@@ -24,39 +22,74 @@ Module Bdd
         End Using
     End Function
 
-    Public Sub NonQuery(sql As String)
+    Public Function NonQuery(sql As String) As Integer
+        Using Req As New MySqlCommand(sql, Connexion)
+            Req.CommandType = CommandType.Text
+            Connexion.Open()
+            Try
+                Return Req.ExecuteNonQuery()
+            Finally
+                Connexion.Close()
+            End Try
+        End Using
+    End Function
 
-    End Sub
+    ' considère que la première colonne est la PK
+    ' renvoie le nombre de ligne concernées (1 ou 0 du coup)
+    Public Function Update(nomtable As String, row As DataRow)
+        Dim sql = "UPDATE " & nomtable & " SET "
+        Dim cmd = New MySqlCommand With {.Connection = Connexion, .CommandType = CommandType.Text}
+
+        Dim tvide = GetTableVide(nomtable)
+
+        Dim pasprem = False
+        For i = 1 To tvide.Columns.Count - 1
+            If pasprem Then sql &= ", " Else pasprem = True
+            Dim nomcol = tvide.Columns(i).Caption
+            sql &= nomcol & " = @" & nomcol
+            cmd.Parameters.AddWithValue("@" & nomcol, (row(i)))
+        Next
+
+        sql &= " WHERE " & tvide.Columns(0).Caption & " = @" & tvide.Columns(0).Caption
+        cmd.Parameters.AddWithValue("@" & tvide.Columns(0).Caption, row(0))
+        cmd.CommandText = sql
+
+        Connexion.Open()
+
+        Try
+            Return cmd.ExecuteNonQuery()
+        Finally
+            Connexion.Close()
+        End Try
+    End Function
 
     Public Function GetTable(nomtable As String) As DataTable
         Return Query("SELECT * FROM " & nomtable)
     End Function
 
-
-    Public Function GetLocVilles() As DataTable
-        'Return
-
+    Public Function GetRow(nomtable As String, nompk As String, id As Integer) As DataRow
+        Dim res = Query("SELECT * FROM " & nomtable & " WHERE " & nompk & "  = " & id)
+        If res.Rows.Count > 0 Then Return res.Rows(0) Else Return Nothing
     End Function
 
-    ' ne met pas la première colonne, en supposant que c'est l'id et qu'il sera généré automatiquement
-    ' sauf si PasAutoId est true
-    Public Function Insert(nomtable As String, row As DataRow, Optional PasAutoId As Boolean = False) As Integer
 
-        Dim rowvide = Query("SELECT * FROM " & nomtable & " WHERE FALSE")   ' récupère une ligne vide, juste pour avoir la structure
+    Public Function Insert(nomtable As String, row As DataRow, Optional PasAutoId As Boolean = False) As Integer
 
         Dim sqlchamps = "("
         Dim sqlvalues = "("
         Dim cmd = New MySqlCommand With {.Connection = Connexion, .CommandType = CommandType.Text}
 
+        Dim tvide = GetTableVide(nomtable)
+
         Dim pasprem = False
-        For i = If(PasAutoId, 0, 1) To rowvide.Columns.Count - 1
+        For i = If(PasAutoId, 0, 1) To tvide.Columns.Count - 1
             If pasprem Then
                 sqlchamps &= ", "
                 sqlvalues &= ", "
             Else
                 pasprem = True
             End If
-            Dim nomcol = rowvide.Columns(i).Caption
+            Dim nomcol = tvide.Columns(i).Caption
             sqlchamps &= nomcol
             sqlvalues &= "@" & nomcol
             cmd.Parameters.AddWithValue("@" & nomcol, (row(i)))
@@ -68,110 +101,77 @@ Module Bdd
         cmd.CommandText = "INSERT INTO " & nomtable & sqlchamps & " VALUES " & sqlvalues
 
         Connexion.Open()
+
+        Dim reader As MySqlDataReader = Nothing
+
         Try
-            Return cmd.ExecuteNonQuery
+            cmd.ExecuteNonQuery()
+            cmd = New MySqlCommand With {.Connection = Connexion, .CommandText = "SELECT LAST_INSERT_ID() FROM " & nomtable}
+            reader = cmd.ExecuteReader
+            reader.Read()
+            Return reader.GetInt32(0)
         Finally
-            Connexion.Close()
-        End Try
-
-<<<<<<< HEAD
-        ' La requête
-        SQL = "select * from t_login where username = '" & user & "' AND password = '" & pass & "'"
-        myCommand.CommandText = SQL
-        myAdapter.SelectCommand = myCommand
-        myData = myCommand.ExecuteReader()
-        myData.Read()
-        Try
-            Dim str = myData.GetString(0)
-            myData.Close()
-            conn.Close()
-            Return str
-        Catch ex As Exception
-            myData.Close()
-            conn.Close()
-            Return 0
-        End Try
-
-
-=======
->>>>>>> origin/master
-    End Function
-
-
-
-    ' Fonctions : - plus forcément à jour
-    ' GetTable(table as String) As DataTable        ' ex : GetTable("demandeurs")
-    ' GetTableWithId(table as String, NomPk As String, Id As Integer) As DataTable      ' ex : GetTableWithId("emplacements","empl_id",4)
-    ' Function GetFormInhumation(id As Integer) As DemandeInhumation
-    ' Function GetFormNvConcession(id As Integer) As DemandeNvConcession
-    ' Function GetFormNvConBenefs(IdForm) As Beneficiaire()
-    ' PutFormInhumation(LeForm As DemandeInhumation) As Integer      ' ajoute un nouveau form en bdd (table forms inhum + éventuellement form nv con & fnvcon bénéficiaires                                                                     renvoie l'id inséré
-    ' Function PutFormNvCon(LeForm As DemandeNvConcession) As Integer
-
-
-    Function Login(user As String, pass As String)
-        Dim sql = "select * from t_login where username = '" & user & "' AND password = '" & pass & "'"
-        Connexion.Open()
-        Try    ' doit fermer la connexion même si erreur
-            Using MyCommand As New MySqlCommand With {.Connection = Connexion, .CommandText = sql},
-                  MyData = MyCommand.ExecuteReader
-                Try     ' doit fermer mydata même si erreur...
-                    MyData.Read()
-                    Return MyData.GetString(0)
-                Finally
-                    MyData.Close()
-                End Try
-            End Using
-        Finally
+            If reader IsNot Nothing AndAlso Not reader.IsClosed Then reader.Close()
             Connexion.Close()
         End Try
     End Function
 
+    ' récupère une table vide, juste pour avoir la structure
+    Function GetTableVide(nomtable As String) As DataTable
+        Return Query("SELECT * FROM " & nomtable & " WHERE FALSE")
+    End Function
 
+    Function GetRowVide(nomtable As String)
+        Return GetTableVide(nomtable).NewRow
+    End Function
+
+    ' met à jour une même colonne dans plusieurs enregistrements, avec la même valeur pour toutes
+    ' ne sert actuellement qu'à enlever ou mettre des défunts dans un emplacement
+    Function UpdateChampPlusieurs(nomtable As String, nompk As String, ids As List(Of Object), nomchamp As String, nouvellevaleur As Object) As Integer
+        If ids.Count = 0 Then Return 0
+        Dim sql As String = ""
+        Dim UneCmd As String
+
+        Using Req As New MySqlCommand With {.CommandType = CommandType.Text, .Connection = Connexion}
+
+            UneCmd = "UPDATE " & nomtable & " SET " & nomchamp & " = @lavaleur WHERE " & nompk & " IN (" & MySqlHelper.EscapeString(Join(ids.ToArray, ", ")) & ")"
+            sql &= UneCmd
+            Req.Parameters.AddWithValue("@lavaleur", nouvellevaleur)
+
+            Req.CommandText = sql
+            Connexion.Open()
+            Try
+                Return Req.ExecuteNonQuery()
+            Finally
+                Connexion.Close()
+            End Try
+        End Using
+    End Function
+
+    Function Login(user As String, pass As String) As Boolean
+        Dim resultat = Query("SELECT * FROM t_login WHERE username = """ & MySqlHelper.EscapeString(user) & """ AND password = " & MySqlHelper.EscapeString(pass))
+        Return resultat.Rows.Count > 0
+    End Function
 
 
     Function GetValeurSql(sql As String, colonne As String) As String
-        'Connexion.ConnectionString = "Server=" & BddHost & ";User Id=" & BddUser & "; Password=" & BddPass & "; Database=" & BddNom & "; Pooling=false"
+        Dim myCommand As New MySqlCommand(sql, Connexion)
+        Dim myData As MySqlDataReader = Nothing
         Connexion.Open()
-        Dim myCommand As New MySqlCommand
-        'Dim myAdapter As New MySqlDataAdapter
-        Dim myData As MySqlDataReader
-        'Dim SQL As String
-        'Dim objConn As New MySql.Data.MySqlClient.MySqlConnection
-        myCommand.Connection = Connexion
-        ' La requête
-        'SQL = commande
-        myCommand.CommandText = sql
-        'myAdapter.SelectCommand = myCommand
-        myData = myCommand.ExecuteReader()
-        myData.Read()
-        Dim str = myData.GetString(colonne)
-        myData.Close()
-        Connexion.Close()
-        Return str
-
+        Try
+            myData = myCommand.ExecuteReader()
+            myData.Read()
+            Dim str = myData.GetString(colonne)
+            Return str
+        Finally
+            If myData IsNot Nothing Then
+                If Not myData.IsClosed Then myData.Close()
+                myData.Dispose()
+            End If
+            myCommand.Dispose()
+            Connexion.Close()
+        End Try
     End Function
 
-    'Public Function InsertPers() As Boolean
-
-    '    Dim conn As MySqlConnection = Connecter()
-    '    Dim sqlCommand As New MySqlCommand
-    '    Dim str_carSql As String
-
-    '    Try
-    '        str_carSql = "insert into personne (id, nom, prenom, naissance, mort, sepulture, enterement, expiration) values ('','heu','peutetre','25','34','65','98','20')"
-    '        MsgBox(str_carSql)
-    '        sqlCommand.Connection = conn
-    '        sqlCommand.CommandText = str_carSql
-    '        sqlCommand.ExecuteNonQuery()
-    '        Return True
-
-    '    Catch ex As Exception
-    '        Return False
-    '        MsgBox("Error occured: Could not insert record")
-    '    End Try
-
-    '    conn.Close()
-    'End Function
 
 End Module
